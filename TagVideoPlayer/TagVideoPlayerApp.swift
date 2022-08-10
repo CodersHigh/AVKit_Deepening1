9177188ac94519846a3ceb15e8bfb0c1d2a361f6
이로운 <lru040409@gmail.com>
2022-08-10 10:21:51 +0900

    프로젝트 셋업

---
+++ TagVideoPlayer/TagVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagVideoPlayerApp.swift
+//  TagVideoPlayer
+//
+//  Created by 이로운 on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct TagVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
